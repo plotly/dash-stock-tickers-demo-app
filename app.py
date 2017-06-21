ed7d3b0b12373e7bbd345a5ebd06bf92e1cfd5ec
alishobeiri <alishobeiri@ymail.com>
2017-06-21 10:21:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def update_graph(tickers):
 
 
 external_css = ["https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i",
-                "https://cdn.rawgit.com/plotly/stylesheets/master/stylesheet.css"]
+                "https://cdn.rawgit.com/plotly/dash-app-stylesheets/2cc54b8c03f4126569a3440aae611bbef1d7a5dd/stylesheet.css"]
 
 for css in external_css:
     app.css.append_css({"external_url": css})
