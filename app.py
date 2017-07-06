d9cdfede85b580130f64e2c1b5d40394bd19247b
alishobeiri <alishobeiri@ymail.com>
2017-07-06 15:37:53 -0400

    Fixed dead logo link

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.layout = html.Div([
                        'margin-top': '20px',
                        'margin-bottom': '0'
                        }),
-        html.Img(src="https://cdn.rawgit.com/plotly/design-assets/a8c0b6972563dfa3e8e7b5d7454d4909fa9db21b/logo/dash/images/dash-logo-by-plotly-stripe.png?token=ARkbwzp9Cq3SoAp8SBfsMVVfotVrJJUxks5ZW_jVwA%3D%3D",
+        html.Img(src="https://cdn.rawgit.com/plotly/dash-app-stylesheets/ce288b65da310bf7a9efe39487061c4b90697fa2/dash-logo-by-plotly-stripe.png",
                 style={
                     'height': '100px',
                     'float': 'right'
